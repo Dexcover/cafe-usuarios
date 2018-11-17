71d9b521251c3528705efd3990808898efc44024
Diego Chicaiza <diegochicaiza1@hotmail.com>
2018-11-17 00:39:06 -0500

    variable de entrono

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@
   if(process.env.NODE_ENV==='dev'){
     urlDB='mongodb://localhost:27017/cafe';
   }else{
-    urlDB='mongodb://cafe-user:admin12345@ds139949.mlab.com:39949/cafe_dexcover';
+    urlDB= process.env.MONGO_URI;
   }
   
  process.env.URLDB = urlDB;
